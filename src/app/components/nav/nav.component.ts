d23186112de8cc4344d51290f82db67f4768221d
Ziadal3tar <ziadalattar22@gmail.com>
2023-03-16 22:08:35 +0200

    'first'

---
+++ src/app/components/nav/nav.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+export class NavComponent {
+  // window.addEventListener('scroll', function () {
+  //   if (scrollY >= 670) {
+  //           document.getElementById('nav').classList.add("bg-green")
+  //   }else if(scrollY <= 670){
+  //       document.getElementById('nav').classList.remove("bg-green")
+  //   }
+  // });
+
+}
